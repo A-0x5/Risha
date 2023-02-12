acbfa6f33ae2deddf5119b6b1811e692a5a4f6be
A-0x5 <114782882+A-0x5@users.noreply.github.com>
2023-02-12 22:52:23 +0300

    Add files via upload

---
+++ Project_Web1/JS/scriptt.js
@@ -0,0 +1,16 @@
+function registor(){
+	var username = decument.getElementById("user");
+	var username = decument.getElementById("pass");
+
+ 
+if (user name==""){
+window.alert("Please enter username");
+return;
+}
+else if (Password==""){
+window.alert("Please enter Password");
+return;	
+}
+else
+	window.location.href="./index.html";
+}
